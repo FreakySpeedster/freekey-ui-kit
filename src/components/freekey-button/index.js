2a53787c340e29f8b2c2f2bfee88dfa814ecd5e9
SRIDHAR MURALI <sridhar.murali95@hotmail.com>
2022-11-23 17:16:51 +0530

    Class Syntax changes

--- src/components/freekey-button/index.js
+++ src/components/freekey-button/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BUTTON_TYPES } from '../../common/data/freekey-button';
 import './freekey-button.css';
 
-const FreekeyButton = (props) => {
-    const { type, text, isInProgress, inProgressText, icon, iconPosition, isDisabled } = props;
-    const getButtonClass = () => {
-        switch (type) {
+class FreekeyButton extends Component {
+    constructor(props){  
+        super(props);
+        this.getButtonClass = this.getButtonClass.bind(this);
+        this.getBtnDisabledState = this.getBtnDisabledState.bind(this);
+    }
+    
+    getButtonClass() {
+        switch (this.props.type) {
             case BUTTON_TYPES.PRIMARY:
                 return 'primaryBtn btn';
             case BUTTON_TYPES.SECONDARY:
@@ -14,16 +19,17 @@ const FreekeyButton = (props) => {
                 return 'tertiaryBtn';
         } 
     }
-    const getBtnDisabledState = () => {
-        return isInProgress || isDisabled ? true : false;
+    getBtnDisabledState() {
+        return this.props.isInProgress || this.props.isDisabled ? true : false;
     }
-    return (
-        <button className={`${getButtonClass()}`} disabled={getBtnDisabledState()} onClick={props.buttonClick} >
-            {icon && iconPosition === 'LEFT' && icon}
-            {isInProgress ? inProgressText : text}
-            {icon && iconPosition === 'RIGHT' && icon}
-        </button>
-    );
+    render() {
+        return (
+            <button className={`${this.getButtonClass()}`} disabled={this.getBtnDisabledState()} onClick={this.props.buttonClick} >
+                {this.props.icon && this.props.iconPosition === 'LEFT' && this.props.icon}
+                {this.props.isInProgress ? this.props.inProgressText : this.props.text}
+                {this.props.icon && this.props.iconPosition === 'RIGHT' && this.props.icon}
+            </button>
+        );
+    }
 }
-
 export default FreekeyButton;
