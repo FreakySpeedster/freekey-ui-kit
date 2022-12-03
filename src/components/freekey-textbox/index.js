9f7081ab9df83fbb457649c8a3101012702b75be
SRIDHAR MURALI <sridhar.murali95@hotmail.com>
2022-12-03 17:43:55 +0530

    Textbox changes

---
+++ src/components/freekey-textbox/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './freekey-textbox.css';
+
+const FreekeyTextbox = (props) => {
+    const { primaryLabel, secondaryLabel, errorText, isError, isDisabled, placeholder } = props;
+    return (
+        <div className="textbox-container">
+            <div className="labels-box">
+                <label className="primary-label">{primaryLabel}</label>
+                {secondaryLabel && <label className="secondary-label">{secondaryLabel}</label>}
+            </div>
+            <div class="input-wrapper">
+                <input className={`textbox ${isError && 'textbox-error'} ${isDisabled && 'mod--disabled'}`} type='text' placeholder={placeholder} disabled={isDisabled} />
+                {isError && <i class="icon"></i>}
+            </div>
+            <div className="error-text">{errorText}</div>
+        </div>
+    );
+}
+export default FreekeyTextbox;
