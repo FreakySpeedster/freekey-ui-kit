f67589b5a45cb8c6fee29715b87d3583370889b2
kusthi <jerrylokesh1999@gmail.com>
2023-08-26 11:17:30 +0530

    Made keys to be unique for tag items

--- src/components/tags-input/index.js
+++ src/components/tags-input/index.js
@@ -10,7 +10,7 @@ const TagsInput = (props) => {
     const handleKeyUp = (event) => {
         if (event.key === 'Enter') {
             if (!tags.includes(inputValue) && inputValue!=='') {
-                setTags([...tags, inputValue]);
+                setTags([...tags, {value: inputValue, id: `${inputValue}-${new Date().getTime()}`}]);
                 setInputValue('');
             }
         } else if (event.key === 'Backspace') {
@@ -24,13 +24,13 @@ const TagsInput = (props) => {
         setInputValue(event.target.value);
     }
     const removeTag = (tag) => {
-        setTags(tags.filter((t) => t !== tag));
+        setTags(tags.filter((t) => t.id !== tag.id));
     }
     
     return (
         <div className={`tags-input ${containerClass ? containerClass : ''}`}>
             {tags.map((tag)=> {
-                return <TagCapsule key={tag} text={tag} tag={tag} showCloseIcon={true} iconPosition={'RIGHT'} clearTag={() => removeTag(tag)} />
+                return <TagCapsule key={tag.id} text={tag.value} tag={tag.value} showCloseIcon={true} iconPosition={'RIGHT'} clearTag={() => removeTag(tag)} />
             })}
             <input type="text" className='active-tags-input' onKeyUp={handleKeyUp} onChange={handleInputChange} placeholder="Add a tag" value={inputValue}/>
         </div>
